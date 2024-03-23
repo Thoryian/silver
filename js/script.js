dc080dfd209c6d4a1c46ff8a05465dfa19daa75a
Thoryian <162287818+Thoryian@users.noreply.github.com>
2024-03-22 22:05:45 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,15 @@ document.addEventListener('click', function () {
   });
   
   document.getElementById('name').textContent = 'Silver';
-  document.getElementById('about').textContent = 'Hi!!! Im Theoryian!. I play games, code, and I am very active on Discord! Thank you for visiting my website! I dont have many socials, but my discord is konciouss, you can friend me there! But please understand I might not accept it. I am not super social outside of Discord and have social anxiety. Please note that Im under 18, so I might not respond to you. Thank you for your understanding!';
-  document.getElementById('stuff').textContent = 'She/Her, 18-, Pansexual, Computer addict, Protogen, Furry, Therian, Gemini, Trans, Unfunny, Low-sleep'
+  document.getElementById('about').textContent = 'Hallo Im a silly wittle furry called Silver, I love music and I love cats, I have three cats Loki, Lucy, and Cleo I love them very much. ❤️taken by Silicon❤️ 2006 Dodge Grand Caravan my beloved ♥️';
+  document.getElementById('stuff').textContent = 'He/Him, 18-, Pansexual, Furry'
 
   const socialLinks = [
-    {name: 'Youtube', url: 'https://www.youtube.com/channel/UCJSPpp8fPp2govWCgn6RXKg'},
-    {name: 'Discord', url: 'https://discord.gg/67F6vHNQDs'},
-    {name: 'Github', url: 'https://github.com/Thoryian'},
-    {name: 'Scratch', url: 'https://scratch.mit.edu/users/theoryian/'},
-    {name: 'Roblox', url: 'https://www.roblox.com/users/4575669522/profile'},
-    {name: 'Pinterest', url: 'https://www.pinterest.com/fluxcore847/'},
-    {name: 'Spotify Playlist', url: 'https://open.spotify.com/playlist/2TOPMi4Xu27kEr8EaGszST?si=26db13c23c684e0c'},
-    {name: 'Steam', url: 'https://steamcommunity.com/profiles/76561199256235424'},
+    {name: 'Youtube', url: 'https://www.youtube.com/@SilverThePanFurry'},
+    {name: 'Scratch', url: 'https://scratch.mit.edu/users/StigmusJR/'},
+    {name: 'Roblox', url: 'https://www.roblox.com/users/864893479/profile'},
+    {name: 'Spotify Playlist', url: 'https://open.spotify.com/playlist/7bWX96xOecc5qbaOVPQzJT?si=bf23a89d4ffa4c23'},
+    {name: 'Steam', url: 'https://steamcommunity.com/profiles/76561199060745984/'},
   ];
   
   const socialList = document.getElementById('social-links');
